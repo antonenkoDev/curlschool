8f590c697310a1ee5db43f637d819d7bf8e96a45
Oleksandr Antonenko <118268661+oantonenko-gbsfo@users.noreply.github.com>
2023-10-14 23:18:01 +0300

    header component update

--- src/app/components/shared/header/header.component.ts
+++ src/app/components/shared/header/header.component.ts
@@ -12,8 +12,23 @@ export class HeaderComponent {
 
   navItems = routes.filter((route) => route.data && route.data.title && route.data.show);
   isMenuOpen = false; // to toggle menu
+  openSubMenus: { [key: string]: boolean } = this.initializeSubMenus(); // To keep track of which submenu is open
 
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  toggleSubMenu(path: string) {
+    this.openSubMenus[path] = !this.openSubMenus[path];
+  }
+
+  private initializeSubMenus(): { [key: string]: boolean } {
+    const subMenus: { [key: string]: boolean } = {};
+    this.navItems.forEach((item) => {
+      if (item.children && item.path) {
+        subMenus[item.path] = false;
+      }
+    });
+    return subMenus;
   }
 }
