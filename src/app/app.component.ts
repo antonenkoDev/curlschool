62816cd7e5dd97e3f924b4ea03aaaf7e1913cf97
Oleksandr Antonenko <118268661+oantonenko-gbsfo@users.noreply.github.com>
2023-11-03 19:05:01 +0200

    remove trash from package.json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { Carousel, initTE } from 'tw-elements';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
